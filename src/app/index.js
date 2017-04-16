d28dd10abe0982629ee06c93d5f3c1f4284b57aa
Jarold Wong <jaroldw@gmail.com>
2017-04-15 22:13:47 -0700

    Fill out leaderboard table

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+require('./css/index.css');
+
 var BoardComponent = React.createClass({
     getInitialState: function () {
-        return { campers: [] }
+        return {
+            campers: []
+        }
     },
     componentDidMount: function() {
         var that = this;
@@ -17,17 +21,44 @@ var BoardComponent = React.createClass({
                     campers: json
                 });
             })
-            .catch(function(err) {
-                // catch errors
-            });
     },
     render: function() {
         return(
             <div>
-                <h1>Leaderboard</h1>
+                <h1>Camper Leaderboard</h1>
+                <Camper campers={this.state.campers} />
             </div>
-        )
+        );
     },
+});
+
+var Camper = React.createClass({
+    render: function() {
+        return(
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Rank</td>
+                        <td>Username</td>
+                        <td>Points in past 30 days</td>
+                        <td>All Time Points</td>
+                    </tr>
+                </tbody>
+            {this.props.campers.map(function(camper, index) {
+                return (
+                    <tbody key={index}>
+                        <tr>
+                            <td>{index + 1}</td>
+                            <td><img className="icon" src={camper.img}/>  {camper.username}</td>
+                            <td>{camper.recent}</td>
+                            <td>{camper.alltime}</td>
+                        </tr>
+                    </tbody>
+                );
+            })}
+            </table>
+        )
+    }
 });
 
 ReactDOM.render(<BoardComponent />, document.getElementById('root'));
