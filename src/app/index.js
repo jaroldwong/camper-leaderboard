4891d786c7f40193ab80ab5e9c711603ce73ebfe
Jarold Wong <jaroldw@gmail.com>
2017-04-15 18:37:16 -0700

    fetch data

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,25 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var BoardComponent = React.createClass({
+    getInitialState: function () {
+        return { campers: [] }
+    },
+    componentDidMount: function() {
+        var that = this;
+
+        fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                that.setState({
+                    campers: json
+                });
+            })
+            .catch(function(err) {
+                // catch errors
+            });
+    },
     render: function() {
         return(
             <div>
